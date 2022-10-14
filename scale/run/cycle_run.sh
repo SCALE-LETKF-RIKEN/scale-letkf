57d79c2b0ce9b7f773c98e765328ef089a7ca621
Takumi Honda <takumi.honda@sci.hokudai.ac.jp>
2022-10-14 12:40:18 +0900

    Adopt to the disk space management with MDT

--- scale/run/cycle_run.sh
+++ scale/run/cycle_run.sh
@@ -115,7 +115,15 @@ if [ "$PRESET" = 'FUGAKU' ]; then
   fi
   TPROC=$((NNODES*PPN))
 
-  VOLUMES="/"$(readlink /data/${GROUP} | cut -d "/" -f 2)
+  CVOLUME=$(pwd | cut -d "/" -f 2) # current volume (e.g., /vol0X0Y or /vol000X)
+  NUM_VOLUME=${CVOLUME:4:1} # get number of current volume 
+
+  if [ "$NUM_VOLUME" = "0" ] ; then
+    VOLUMES=${CVOLUME}
+  else
+    VOLUMES="/vol000${NUM_VOLUME}"
+  fi
+
   if [ $VOLUMES != "/vol0004" ] ;then
     VOLUMES="${VOLUMES}:/vol0004" # spack
   fi
